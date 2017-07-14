f873fcfce33688df0a8f2daad2c0028f67340e8d
pcraner <semagon88@gmail.com>
2017-07-13 22:14:18 -0600

    Add files via upload

---
+++ Repairs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGM_Checkout_dev.Controllers
+{
+    public class Repairs
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var repair = from r in _context.Inventory
+                         .Include(r => r.Status)
+                       select r;
+
+            return View(await repair.AsNoTracking().ToListAsync());
+        }
+
+        // GET: Rentals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //linked the rental to inventory with .include
+            var repair = await _context.Inventory
+                .Include(r => r.Status)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.InventoryID == id);
+            repair = repair.where(i.rentalreturndate > i.rentalreturndate || i.rentallatefeepaid == false);
+
+            return View(repair);
+        }
+    }
+}
---
+++ latefees.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DGM_Checkout_dev.Controllers
+{
+    public class latefees
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var user = from r in _context.User
+                         .Include(r => r.Rental)
+                         select r;
+
+            return View(await user.AsNoTracking().ToListAsync());
+        }
+
+        // GET: Rentals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            //linked the rental to inventory with .include
+            var users = await _context.User
+                .Include(r => r.Rental)
+                .AsNoTracking()
+                .SingleOrDefaultAsync(m => m.RentalID == id);
+            users = users.where(i.rentalreturndate > i.rentalreturndate || i.rentallatefeepaid == false);
+
+            return View(users);
+        }
+
+    }
+}
