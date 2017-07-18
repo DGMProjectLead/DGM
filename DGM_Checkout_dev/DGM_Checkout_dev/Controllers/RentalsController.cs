e0cbfb4ed412edf6456923d24dc6192a3d0998b4
Ken <wisteriahillmedia@gmail.com>
2017-07-17 20:07:43 -0600

    pushing clone of 7/15/17 repo state to master
    the SHA of the clone being pushed is 842f947887dfa1a7a580c113af35f3d4f58dda52

--- DGM_Checkout_dev/DGM_Checkout_dev/Controllers/RentalsController.cs
+++ DGM_Checkout_dev/DGM_Checkout_dev/Controllers/RentalsController.cs
@@ -23,57 +23,14 @@ namespace DGM_Checkout_dev.Controllers
 
         // GET: Rentals
         /// <summary>
-        /// Added search functions to index method
-        /// Search can likely be moved to it's own Search method or partial view
-        /// This cuts down on clutter in Index
+        /// 
         /// </summary>
-        /// <param name="nameSearch"></param>
-        /// <param name="userSearch"></param>
-        /// <param name="locationSearch"></param>
-        /// <param name="checkoutSearch"></param>
-        /// <param name="dueSearch"></param>
-        /// <param name="returnSearch"></param>
-        /// <returns>Returns database entries based on the selected search inputs</returns>
-        public async Task<IActionResult> Index(string nameSearch, string userSearch, string locationSearch, string checkoutSearch, string dueSearch, string returnSearch)
-        {
-            ViewData["nameSearch"] = nameSearch;
-            ViewData["userSearch"] = userSearch;
-            ViewData["locationSearch"] = locationSearch;
-            ViewData["checkoutSearch"] = checkoutSearch;
-            ViewData["dueSearch"] = dueSearch;
-            ViewData["returnSearch"] = returnSearch;
-
-
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {              
             var rental = from r in _context.Rental
                          .Include(r => r.User)
                          select r;
-            if(!String.IsNullOrEmpty(nameSearch))
-            {
-                rental = rental.Where(r => r.RentalName.Contains(nameSearch));
-            }
-            if(!String.IsNullOrEmpty(userSearch))
-            {
-                rental = rental.Where(r => r.User.UVID.Contains(userSearch));
-            }
-            if(!String.IsNullOrEmpty(locationSearch))
-            {
-                rental = rental.Where(r => r.RentalLocation.Contains(locationSearch));
-            }
-            if(!String.IsNullOrEmpty(checkoutSearch))
-            {
-                DateTime checkoutConvert = Convert.ToDateTime(checkoutSearch);
-                rental = rental.Where(r => r.RentalCheckoutDate == checkoutConvert);
-            }
-            if(!String.IsNullOrEmpty(dueSearch))
-            {
-                DateTime dueConvert = Convert.ToDateTime(dueSearch);
-                rental = rental.Where(r => r.RentalDueDate == dueConvert);
-            }
-            if(!String.IsNullOrEmpty(returnSearch))
-            {
-                DateTime returnConvert = Convert.ToDateTime(returnSearch);
-                rental = rental.Where(r => r.RentalReturnDate == returnConvert);
-            }
 
             return View(await rental.AsNoTracking().ToListAsync());
         }
@@ -120,14 +77,14 @@ namespace DGM_Checkout_dev.Controllers
 
             if (ModelState.IsValid)
             {
-                //rental.RentalName = rental.User.UVID + rental.RentalCheckoutDate.ToString();
                 _context.Add(rental);
                 await _context.SaveChangesAsync();
-                return RedirectToAction("Index");  //"Edit/" + rental.RentalID);
+                return RedirectToAction("AddItems/" + rental.RentalID);  //"Edit/" + rental.RentalID);
             }
            
             ViewData["UserID"] = new SelectList(_context.User, "UserID", "UserFullInfo", rental.UserID);
             return View(rental);
+
         }
 
         // GET: Rentals/Edit/5
@@ -138,7 +95,9 @@ namespace DGM_Checkout_dev.Controllers
                 return NotFound();
             }
 
-            var rental = await _context.Rental.SingleOrDefaultAsync(m => m.RentalID == id);
+            var rental = await _context.Rental
+                .Include(r => r.User)
+                .SingleOrDefaultAsync(m => m.RentalID == id);
             if (rental == null)
             {
                 return NotFound();
@@ -176,7 +135,6 @@ namespace DGM_Checkout_dev.Controllers
                 {
                     ModelState.AddModelError("", "Unable to save changes.  Try again and if problems continue call IT support.");
                 }
-
             }
 
             ViewData["UserID"] = new SelectList(_context.User, "UserID", "UserFullInfo", rentalUpdate.UserID);
@@ -217,7 +175,6 @@ namespace DGM_Checkout_dev.Controllers
         {
             return _context.Rental.Any(e => e.RentalID == id);
         }
-<<<<<<< HEAD
 
         public async Task<IActionResult> Search(string nameSearch, string userSearch, string locationSearch, string checkoutSearch, string dueSearch, string returnSearch, bool feePaid, bool feeDue)
         {
@@ -290,7 +247,5 @@ namespace DGM_Checkout_dev.Controllers
 
             return View(rental);
         }
-=======
->>>>>>> parent of 4583432... Integrated the Reports section.
     }
 }
